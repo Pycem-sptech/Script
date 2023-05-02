7adaec9ea8467690eed18c6289292660e0720652
Marcos Fernando Araújo da Paixão <110927325+marcos-nnd@users.noreply.github.com>
2023-05-02 19:44:26 -0300

    Alterações e correções no código, pendencia no repositorio escolhido e arquivo desatualizado

--- script.sh
+++ script.sh
@@ -1,7 +1,25 @@
-java version
+java -version
 
-if [ $? != 0 ];
+if [ $? == 0 ];
     then
+        echo "Cliente já possui o java instalado"
+        echo "Deseja fazer a instalação do aplicativo Pycem? [s/n]"
+        read resp
+
+        if [ \"$resp\" == \"s\" ];
+            then
+                git clone https://github.com/Pycem-sptech/pycemJar.git
+                echo "Pycem instalado!"
+                echo "Deseja executá-lo? [s/n]"
+            read resp2
+
+            if [ \"$resp2\" == \"s\" ];
+                    then
+                    cd #Diretório em que o arquivo Pycem está alocado
+                    java -jar #Arquivo pycem.jar atualizado
+            fi
+        fi
+    else
         echo "Cliente ainda não tem o java instalado nessa máquina"
         echo "Deseja instalar o java? [s/n]"
         read get
@@ -9,23 +27,24 @@ if [ $? != 0 ];
         if [ \"$get\" == \"s\" ];
             then
                 sudo apt install openjdk-17-jre -y
+        
+            echo "O java foi instalado com sucesso!"
+            echo "Deseja fazer a instalação do aplicativo Pycem? [s/n]"
+            read resp3
+
+            if [ \"$resp3\" == \"s\" ];
+                then
+                    git clone https://github.com/Pycem-sptech/Script-Insta-o.git
+                    echo "Pycem instalado!"
+                    echo "Deseja executá-lo? [s/n]"
+                read resp4
+
+                if [ \"$resp4\" == \"s\" ];
+                    then
+                        cd #Diretório em que o arquivo Pycem está alocado
+                        java -jar #Arquivo pycem.jar atualizado
+                fi
+            fi
         fi
-fi
 
-echo "Cliente já possui o java instalado"
-echo "Deseja fazer a instalação do arquivo .jar? [s/n]"
-read resp
-if [ \"$resp\" == \"s\" ];
-           
-    then
-         git clone https://github.com/Pycem-sptech/pycemJar.git
-         echo "Instalação concluida!"
-         echo "Deseja executá-lo? [s/n]"
-         read resp2
-
-    if [ \"$resp2\" == \"s\" ];
-       then
-       cd pycemJar/
-       java -jar pycem.jar
-    fi
 fi
