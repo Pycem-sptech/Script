34bc2baa3f14a1210ef122972120bbfb89402243
pedro-l0pes <110927673+pedro-l0pes@users.noreply.github.com>
2023-05-01 21:44:52 -0300

    Modificando Script
    Arrumando o Script de instalação

--- script.sh
+++ script.sh
@@ -1,25 +1,7 @@
 java version
 
-if [ $? == 0 ];
+if [ $? != 0 ];
     then
-        echo "Cliente já possui o java instalado"
-        echo "Deseja fazer a instalação do arquivo .jar? [s/n]"
-        read resp
-
-        if [ \"$resp\" == \"s\" ];
-            then
-                git clone https://github.com/Pycem-sptech/pycemJar.git
-                echo "Instalação concluida!"
-                echo "Deseja executá-lo? [s/n]"
-            read resp2
-
-            if [ \"$resp2\" == \"s\" ];
-                    then
-                    cd pycemJar/
-                    java -jar pycem.jar
-            fi
-        fi
-    else
         echo "Cliente ainda não tem o java instalado nessa máquina"
         echo "Deseja instalar o java? [s/n]"
         read get
@@ -28,4 +10,22 @@ if [ $? == 0 ];
             then
                 sudo apt install openjdk-17-jre -y
         fi
+fi
+
+echo "Cliente já possui o java instalado"
+echo "Deseja fazer a instalação do arquivo .jar? [s/n]"
+read resp
+if [ \"$resp\" == \"s\" ];
+           
+    then
+         git clone https://github.com/Pycem-sptech/pycemJar.git
+         echo "Instalação concluida!"
+         echo "Deseja executá-lo? [s/n]"
+         read resp2
+
+    if [ \"$resp2\" == \"s\" ];
+       then
+       cd pycemJar/
+       java -jar pycem.jar
+    fi
 fi
